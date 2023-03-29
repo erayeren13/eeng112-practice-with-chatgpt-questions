e93481dd880dbbce13e7d92703143e23ed7bbee1
Eray <101010198+erayeren13@users.noreply.github.com>
2023-03-30 01:38:15 +0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,46 @@
+//Eray EREN -- 29.03.23 -- EENG112 Practice
+//On this page I will solve problems which Chat-GPT gives me
+
+#include <stdio.h>
+
+/*  1- Write a C program that takes the sum of two numbers. 
+	The program should get two numbers from the user and the result screen should be printed.
+	
+int main()
+{
+	int num1,num2,sum;
+	printf("Please enter 2 number \n");
+	scanf("%d %d",&num1,&num2);
+	sum=num1+num2;
+	printf("%d",sum);
+return 0;	
+}
+
+*/
+
+
+/*  2- Write a C program that calculates the factorial of a number received from the user.
+	
+int main()
+{
+	int num1,i,result,fac;
+	printf("Please enter the number that you want to calculate as factorial \n");
+	scanf("%d",&num1);
+	fac=num1;
+	if(num1<0)
+		printf("You can not enter negative integer");
+	else if(num1==0)
+		printf("0! = 1");
+	else
+		for(i=num1;i>0;i--)
+		{fac=fac*i;}
+		result=fac/num1;
+		printf("%d! = %d",num1,result);
+			
+return 0;	
+}
+
+ */
+
+
+
